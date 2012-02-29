c968e59320a2a53126c3fd78e2e8d60c54210e8a
David Trainer <david@minimalmedia.com>
2012-02-29 10:33:09 -0800

    Add sample images and gallery; fix default colorbox settings

--- js/mm_image.js
+++ js/mm_image.js
@@ -2,7 +2,11 @@
 
   $(function() {
     // Handler for .ready() called.
-    $("a.image-gallery").colorbox();
+    $("a.image-gallery").colorbox({
+      rel:'image-gallery',
+      maxHeight:'100%',
+      maxWidth:'100%',
+    });
   });
 
 })(jQuery);
